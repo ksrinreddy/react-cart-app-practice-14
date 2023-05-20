1ead1142d8cc327e702aca1f4976ede79802e3c9
ksrinreddy <ksrinreddy@gmail.com>
2023-05-20 14:21:41 +0530

    built app

---
+++ src/reducer.js
@@ -0,0 +1,77 @@
+import React from "react";
+
+const reducer = (state, action) => {
+  if (action.type === "CLEAR_CART") {
+    return { ...state, cart: [] };
+  }
+  if (action.type === "REMOVE") {
+    return {
+      ...state,
+      cart: state.cart.filter((cartItem) => cartItem.id !== action.payload),
+    };
+  }
+  if (action.type === "INCREASE") {
+    let tempCart = state.cart.map((cartItem) => {
+      if (cartItem.id === action.payload) {
+        return { ...cartItem, amount: cartItem.amount + 1 };
+      } else {
+        return cartItem;
+      }
+    });
+    return { ...state, cart: tempCart };
+  }
+  if (action.type === "DECREASE") {
+    let tempCart = state.cart
+      .map((cartItem) => {
+        if (cartItem.id === action.payload) {
+          return { ...cartItem, amount: cartItem.amount - 1 };
+        } else {
+          return cartItem;
+        }
+      })
+      .filter((cartItem) => cartItem.amount !== 0);
+    return { ...state, cart: tempCart };
+  }
+  if (action.type === "GET_TOTALS") {
+    let { total, amount } = state.cart.reduce(
+      (cartTotal, cartItem) => {
+        const { price, amount } = cartItem;
+        const itemTotal = price * amount;
+
+        cartTotal.total += itemTotal;
+        cartTotal.amount += amount;
+        return cartTotal;
+      },
+      { total: 0, amount: 0 }
+    );
+    total = parseFloat(total.toFixed(2));
+    return { ...state, total, amount };
+  }
+  if (action.type === "LOADING") {
+    return { ...state, loading: true };
+  }
+  if (action.type === "DISPLAY_ITEMS") {
+    return { ...state, cart: action.payload, loading: false };
+  }
+  // (if you want to) combine both increase and decrese functions into one function (to avoid the repeatation of code in reducer function)
+  if (action.type === "TOGGLE_AMOUNT") {
+    let tempCart = state.cart
+      .map((cartItem) => {
+        if (cartItem.id === action.payload.id) {
+          if (action.payload.type === "inc") {
+            return { ...cartItem, amount: cartItem.amount + 1 };
+          } else if (action.payload.type === "dec") {
+            return { ...cartItem, amount: cartItem.amount - 1 };
+          }
+        } else {
+          return cartItem;
+        }
+      })
+      .filter((cartItem) => cartItem.amount !== 0);
+
+    return { ...state, cart: tempCart };
+  }
+  return state;
+};
+
+export default reducer;
